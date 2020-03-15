9e98e128802ae3ae8c36a815e0b3a550c034d3d7
newssite <khadkasuraj6584@gmail.com>
2020-03-15 13:08:45 +0545

    laravelProject

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+</head>
+<body>
+    <h1>Users</h1>
+    <br>
+
+    <ul>
+        @foreach($users as $user)
+            <li>{{ $user->name }}</li>
+        @endforeach()
+    </ul>
+    {{ $users->links() }}
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Admin\UsersController@index'); //for namespace, use backslash
